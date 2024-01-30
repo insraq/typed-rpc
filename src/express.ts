c3bb48a7f0c04d96f4da2d1c6cbef2b60ac578c2
insraq <ruoysun@gmail.com>
2024-01-30 21:38:02 +0200

    Log error

--- src/express.ts
+++ src/express.ts
@@ -1,22 +1,21 @@
 import type { Request, RequestHandler } from "express";
 import { handleRpc } from "./server";
 
-
 export interface ServiceFactory {
-  (req: Request): object;
+	(req: Request): object;
 }
 
 export function rpcHandler(serviceOrFactory: object | ServiceFactory) {
-  const handler: RequestHandler = (req, res, next) => {
-    const service =
-      typeof serviceOrFactory === "function"
-        ? serviceOrFactory(req)
-        : serviceOrFactory;
+	const handler: RequestHandler = (req, res, next) => {
+		const service =
+			typeof serviceOrFactory === "function"
+				? serviceOrFactory(req)
+				: serviceOrFactory;
 
-    handleRpc(req.body, service)
-      .then((result) => res.json(result))
-      .catch(next);
-  };
+		handleRpc(req.body, service)
+			.then((result) => res.json(result))
+			.catch(next);
+	};
 
-  return handler;
+	return handler;
 }
